77e13ee45e01b76ec48c224c7cae71a5fd2043d0
SamuelCelisL <samuelcelislizcano@gmail.com>
2024-04-26 10:32:26 -0500

    Ajuste de colores y gradientes

--- Src/Components/login_GenList/login.py
+++ Src/Components/login_GenList/login.py
@@ -2,7 +2,7 @@ import sys
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit,
                              QPushButton, QHBoxLayout, QVBoxLayout)
-from PyQt6.QtGui import QPixmap, QFont
+from PyQt6.QtGui import QPixmap, QFont, QLinearGradient, QColor
 from PyQt6 import QtGui, QtCore
 
 
@@ -52,14 +52,15 @@ class login (QWidget):
                     max-width: 60px;
                 }""")
         self.usuario_input = QLineEdit()
-        self.usuario_input.setFixedWidth(300)
         self.usuario_input.setStyleSheet("""QLineEdit{
                     color: black;
                     font-family: sans-serif;
                     border: 1px solid black;
-                    max-width: 300px;
+                    max-width: 200px;
                 }""")
         self.usuario_input.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.usuario_input.setPlaceholderText(
+            "Escribe tu usuario")
 
         Contra_label = QLabel("CONTRASEÑA")
         Contra_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -72,38 +73,46 @@ class login (QWidget):
                     max-width: 100px;
                 }""")
         self.Contra_input = QLineEdit()
-        self.Contra_input.setFixedWidth(300)
         self.Contra_input.setEchoMode(QLineEdit.EchoMode.Password)
         self.Contra_input.setStyleSheet("""QLineEdit{
                     color: black;
                     font-family: sans-serif;
+                    border-radius: 50px;
                     border: 1px solid black;
-                    max-width: 300px;
+                    max-width: 200px;
                 }""")
         self.Contra_input.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.Contra_input.setPlaceholderText("Escribe tu contraseña")
 
+        stilo_credito = """QLabel{
+                    color: white;
+                    font-family: sans-serif;
+                    font-weight: bold;
+                    font-size: 20px;
+        }
+"""
         cretido = QLabel("Desarrollado por:")
         cretido.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        cretido.setFont(QFont('Times New Roman', 20))
+        # cretido.setFont(QFont('sans-serif', 20))
         cretido.setAttribute(
             QtCore.Qt.WidgetAttribute.WA_TranslucentBackground, True)
-        cretido.setStyleSheet('color: white')
+        cretido.setStyleSheet(stilo_credito)
 
         autor1 = QLabel("Juan Pablo Marquez Sanchez")
         autor1.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        autor1.setFont(QFont('Times New Roman', 10))
+        autor1.setFont(QFont('sans-serif', 10))
         autor1.setStyleSheet('color: white')
         autor1.setAttribute(
             QtCore.Qt.WidgetAttribute.WA_TranslucentBackground, True)
         autor2 = QLabel("Samuel Andres Celis Lizcano")
         autor2.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        autor2.setFont(QFont('Times New Roman', 10))
+        autor2.setFont(QFont('sans-serif', 10))
         autor2.setStyleSheet('color: white')
         autor2.setAttribute(
             QtCore.Qt.WidgetAttribute.WA_TranslucentBackground, True)
         autor3 = QLabel("Yorman Rodolfo Rodriguez Jaimes")
         autor3.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        autor3.setFont(QFont('Times New Roman', 10))
+        autor3.setFont(QFont('sans-serif', 10))
         autor3.setStyleSheet('color: white')
         autor3.setAttribute(
             QtCore.Qt.WidgetAttribute.WA_TranslucentBackground, True)
@@ -113,7 +122,7 @@ class login (QWidget):
         boton_registrar.setFixedWidth(150)
         boton_registrar.setStyleSheet("""
                     QPushButton {
-                        background-color: #0074D9;
+                        background-color: #444847;
                         color: white;
                         border-radius: 5px;
                         padding: 5px;
@@ -144,7 +153,7 @@ class login (QWidget):
         # gradient_titulo.setColorAt(0.1, QtGui.QColor(46, 145, 221))
         # gradient_titulo.setColorAt(0.4, QtGui.QColor(40, 125, 190))
         # gradient_titulo.setColorAt(0.6, QtGui.QColor(33, 104, 158))
-        diseño_titulo = """background: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0.1 rgba(46, 145, 221, 255), 
+        diseño_titulo = """background: qlineargradient(x1:0, y1:0, x2:0.707, y2:0.707, stop:0.1 rgba(46, 145, 221, 255), 
             stop:0.4 rgba(40, 125, 190, 255), 
             stop:0.6 rgba(33, 104, 158, 255));"""
         widget_contenedor_titulo.setLayout(contenedor_titulo)
@@ -156,10 +165,9 @@ class login (QWidget):
         widget_contenedor_registro = QWidget()
         widget_contenedor_registro.setLayout(contenedor_registro)
         widget_contenedor_registro.setStyleSheet("""QWidget{
-                                    background-color: white;
+                                    background-color: #DBE5D9;
                                     border: 1px solid black;
                                     }""")
-        widget_contenedor_registro.setGeometry(0, 0, 400, 500)
 
         contenedor_logo = QHBoxLayout()
         contenedor_usuario = QHBoxLayout()
@@ -168,11 +176,14 @@ class login (QWidget):
         contenedor_imput_contra = QHBoxLayout()
         # Parte Azul ↓
         contenedor_credito = QVBoxLayout()
-
         # gradient_barra_F = QtGui.QLinearGradient(0, 0, 0, 400)
         # gradient_barra_F.setColorAt(0.0, QtGui.QColor(33, 104, 158))
         # gradient_barra_F.setColorAt(1.0, QtGui.QColor(0, 0, 0))
-        diseño_barra_F = "background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgba(33, 104, 158, 255), stop: 1 rgba(0, 0, 0, 255))"
+        diseño_barra_F = "background: qlineargradient(x1: 0, y1: 0, x2: 0.707, y2: 0.707, stop: 0.04 #45DC20, stop: 0.6 #179400)"
+        # diseño_barra_F = """background: qlineargradient(x1:0, y1:0, x2:0.707, y2:0.707, stop:0.1 rgba(46, 145, 221, 255),
+        #     stop:0.4 rgba(40, 125, 190, 255),
+        #     stop:0.6 rgba(33, 104, 158, 255));"""
+        # diseño_barra_F = "background-color: #0074D9;"
 
         widget_contenedor_credito = QWidget()
         widget_contenedor_credito.setLayout(contenedor_credito)
