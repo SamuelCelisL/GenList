9187e410d2d8d8a70b86cc2a777d4c9e0a837c47
SamuelCelisL <samuelcelislizcano@gmail.com>
2024-06-10 17:19:14 -0500

    se intrudujeron algunas funciones con base de datos

--- Src/components/conexcionBD.py
+++ Src/components/conexcionBD.py
@@ -61,3 +61,151 @@ def obtener_id_profesor(documento_profesor):
     cursor.close()
     conexion.close()
     return profesor_id
+
+#!HOJA DE WORD 2
+
+
+def insertar_clase(nombre_clase, profesor_id):
+    conexion = sqlite3.connect('src/DataBase/BaseDeDatos.db')
+    cursor = conexion.cursor()
+
+    # Preparar la sentencia SQL para insertar la clase
+    cursor.execute('''
+        INSERT INTO CLASES (Nombre_Clase, Profesor_ID)
+        VALUES (?, ?)
+    ''', (nombre_clase, profesor_id))
+
+    # Guardar los cambios en la base de datos
+    conexion.commit()
+    cursor.close()
+    conexion.close()
+
+
+def obtener_id_clase(nombre_clase):
+    conexion = sqlite3.connect('src/DataBase/BaseDeDatos.db')
+    cursor = conexion.cursor()
+
+    # Buscar el ID_Clase en la base de datos
+    cursor.execute('''
+        SELECT ID_Clase
+        FROM CLASES
+        WHERE Nombre_Clase = ?
+    ''', (nombre_clase,))
+    resultado = cursor.fetchone()
+
+    # Si se encontró un registro, extraer el ID_Clase
+    if resultado:
+        clase_id = resultado[0]
+    else:
+        clase_id = None
+
+    # Retornar el ID_Clase o None
+    cursor.close()
+    conexion.close()
+    return clase_id
+
+#!Hoja de WORD 3
+
+
+def insertar_estudiante(documento_estudiante, nombre_estudiante, carrera_estudiante, clase_id):
+    conexion = sqlite3.connect('src/DataBase/BaseDeDatos.db')
+    cursor = conexion.cursor()
+
+    # Preparar la sentencia SQL para insertar el estudiante
+    cursor.execute('''
+        INSERT INTO ESTUDIANTES (Documento_Estudiante, Nombre_Estudiante, Carrera_Estudiante, Clase_ID)
+        VALUES (?, ?, ?, ?)
+    ''', (documento_estudiante, nombre_estudiante, carrera_estudiante, clase_id))
+
+    # Guardar los cambios en la base de datos
+    conexion.commit()
+    cursor.close()
+    conexion.close()
+
+
+def insertar_datos_biometricos(datos_biometricos, clase_id):
+    conexion = sqlite3.connect('src/DataBase/BaseDeDatos.db')
+    cursor = conexion.cursor()
+    # Preparar la sentencia SQL para insertar los datos biométricos
+    cursor.execute('''
+        INSERT INTO DATOS_BIOMETRICOS (Datos_Biometricos, Clase_ID)
+        VALUES (?, ?)
+    ''', (datos_biometricos, clase_id))
+    # Guardar los cambios en la base de datos
+    conexion.commit()
+    cursor.close()
+    conexion.close()
+
+#! Funciones de WhatsApp
+
+
+def obtener_nombres_clases(profesor_id):
+    conexion = sqlite3.connect('src/DataBase/BaseDeDatos.db')
+    cursor = conexion.cursor()
+
+    # Buscar los nombres de las clases del profesor
+    print(profesor_id)
+    cursor.execute('''
+        SELECT Nombre_Clase
+        FROM CLASES
+        WHERE Profesor_ID = ?
+    ''', (profesor_id,))
+    resultado = cursor.fetchall()
+
+    # Extraer los nombres de las clases del resultado
+    nombres_clases = []
+    for registro in resultado:
+        nombres_clases.append(registro[0])
+
+    print(nombres_clases)
+    # Retornar el array con los nombres de las clases
+    cursor.close()
+    conexion.close()
+    return nombres_clases
+
+
+def obtener_estudiante_por_posicion(clase_id, posicion):
+    conexion = sqlite3.connect('src/DataBase/BaseDeDatos.db')
+    cursor = conexion.cursor()
+
+    # Buscar los datos del estudiante en la posición especificada
+    cursor.execute('''
+        SELECT Documento_Estudiante, Nombre_Estudiante, Carrera_Estudiante
+        FROM ESTUDIANTES
+        WHERE Clase_ID = ?
+        ORDER BY ID_Estudiante
+        LIMIT 1 OFFSET ? - 1
+    ''', (clase_id, posicion))
+    resultado = cursor.fetchone()
+
+    # Extraer los datos del estudiante del resultado
+    if resultado:
+        estudiante = (resultado[0], resultado[1], resultado[2])
+    else:
+        estudiante = None
+
+    # Retornar el array con los datos del estudiante o None si no se encuentra
+    cursor.close()
+    conexion.close()
+    return estudiante
+
+
+def obtener_estudiantes_de_una_clase(clase_id, estudiantes):
+    conexion = sqlite3.connect('src/DataBase/BaseDeDatos.db')
+    cursor = conexion.cursor()
+
+    # Obtener la cantidad total de estudiantes en la clase
+    cursor.execute(
+        'SELECT COUNT(*) FROM ESTUDIANTES WHERE Clase_ID = ?', (clase_id,))
+    total_estudiantes = cursor.fetchone()[0]
+
+    # Recorrer los estudiantes de la clase y agregarlos a la lista
+    for posicion in range(1, total_estudiantes + 1):
+        estudiante = obtener_estudiante_por_posicion(clase_id, posicion)
+        if estudiante:
+            estudiantes.append(estudiante)
+
+    # Retornar la lista de estudiantes actualizada
+    cursor.close()
+    conexion.close()
+    return estudiantes
--- Src/inicio.py
+++ Src/inicio.py
@@ -2,7 +2,7 @@ import sys
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
     QApplication, QWidget, QLabel, QHBoxLayout, QVBoxLayout, QPushButton,
-    QLineEdit, QMessageBox, QSizePolicy, QScrollArea, QTableWidget, QHeaderView)
+    QLineEdit, QMessageBox, QSizePolicy, QScrollArea, QTableWidget, QHeaderView, QTableWidgetItem)
 from PyQt6.QtGui import QFont, QIcon
 from PyQt6 import QtGui, QtCore
 from components import login, conexcionBD
@@ -18,6 +18,8 @@ class inicio (QWidget):
         screen = app.primaryScreen()
         self.ventana = screen.size()
         self.profesor_id = None
+        self.clase_id = None
+        self.estudiantes = []
         self.setWindowTitle("Login GenList")
         self.setWindowIcon(QIcon('src/images/logo2.ico'))
         self.generar_formulario()
@@ -125,33 +127,33 @@ class inicio (QWidget):
         self.boton_registrar.clicked.connect(self.haz_dado_click)
         # self.boton_registrar.clicked.connect(self.cambiar_pantalla)
 
-        # Creacion de los botones de las barras de las paginas
-        # botones pag1
+        #! Creacion de los botones de las barras de las paginas
+        # todo botones pag1
         self._boton_cerrar_sesion = QPushButton("Cerrar Sesion")
         self._boton_cerrar_sesion.clicked.connect(self.volver_inicio)
         self.boton_crear_curso = QPushButton("Crear Curso")
         self.boton_crear_curso.clicked.connect(self.crear_curso)
-        # botones pag2
+        # todo botones pag2
         self.boton_cancelar = QPushButton("Cancelar")
         self.boton_cancelar.clicked.connect(self.cancelar)
         self.boton_agregar_estudiante = QPushButton("Agregar Estudiante")
         self.boton_agregar_estudiante.clicked.connect(self.b_llenar_curso)
         self.boton_finalizar = QPushButton("Finalizar")
         self.boton_finalizar.clicked.connect(self.finalizar_curso)
-        # self.boton_finalizar.clicked.connect()
-        # botones pag3
+        # todo botones pag3
         self.boton_cancelar2 = QPushButton("Cancelar")
         self.boton_cancelar2.clicked.connect(self.volverpag2)
         self.boton_biometria = QPushButton("Biometria")
         self.boton_guardar = QPushButton("Registrar")
+        self.boton_guardar.clicked.connect(self.creacion_vector)
         self.boton_guardar.clicked.connect(self.registrar_estudiante)
-        # botones pag4
+        # todo botones pag4
         self.boton_cancelar3 = QPushButton("Cancelar")
         self.boton_cancelar3.clicked.connect(self.volver_pag_cursos)
         self.boton_asistio = QPushButton("Registrar Estudiante")
         self.boton_pdf = QPushButton("Generar PDF")
 
-        # LAYOUT DE LOS CREDITOS ↓↓↓↓
+        #! LAYOUT DE LOS CREDITOS ↓↓↓↓
         contenedor_credito = QVBoxLayout()
 
         diseño_barra_F = F"""QWidget {{
@@ -163,7 +165,7 @@ class inicio (QWidget):
         widget_contenedor_credito = QWidget()
         widget_contenedor_credito.setLayout(contenedor_credito)
         widget_contenedor_credito.setStyleSheet(diseño_barra_F)
-        # LAYOUT DE LOS CREDITOS ↑↑↑↑
+        #! LAYOUT DE LOS CREDITOS ↑↑↑↑
 
         # AGREGAR ELEMENTOS A LOS LAYOUTS
         # LAYOUT TITULO ↓
@@ -173,7 +175,7 @@ class inicio (QWidget):
         contenedor_credito.addWidget(autor1)
 
         fondo = QVBoxLayout()
-# Agregar los Qwidget a al Layout original
+    # Agregar los Qwidget a al Layout original
         contenedor_principal.addWidget(widget_contenedor_titulo)
 
         contenedor_principal.addWidget(
@@ -181,16 +183,16 @@ class inicio (QWidget):
 
         contenedor_principal.addWidget(widget_contenedor_credito)
 
-# Agregar los layouts a al layoud original
+    # Agregar los layouts a al layoud original
         fondo.addWidget(widget_contenedor_principal)
         app.setStyle("Fusion")
         # Esta linea crea el primer layout ↓
         self.setLayout(fondo)
 
     def generar_cursos(self, boton_cerrar, boton_crear_curso):
-        # print("ESTAS EN LA SEGUNDA VENTANA")
-        # print("Ancho", self.ventana.width())
-        # print("Alto", self.ventana.height())
+
+        print(self.profesor_id)
+        materias = conexcionBD.obtener_nombres_clases(self.profesor_id)
         general = QVBoxLayout()
         widget_general = QWidget()
         widget_general.setLayout(general)
@@ -217,18 +219,18 @@ class inicio (QWidget):
                                     }""")
         scroll_area.setWidget(widget_scroll)
 
-        materias = [
-            "Sistemas Inteligentes AR",
-            "Fundamentos de computacion paralela y distribuida AR",
-            "ingenieria de Software I BR",
-            "REDES BR",
-            "Fundamentos de programacion AR",
-            "Sistemas Inteligentes AR",
-            "Fundamentos de computacion paralela y distribuida AR",
-            "ingenieria de Software I BR",
-            "REDES BR",
-            "Fundamentos de programacion AR"
-        ]
+        # materias = [
+        #     "Sistemas Inteligentes AR",
+        #     "Fundamentos de computacion paralela y distribuida AR",
+        #     "ingenieria de Software I BR",
+        #     "REDES BR",
+        #     "Fundamentos de programacion AR",
+        #     "Sistemas Inteligentes AR",
+        #     "Fundamentos de computacion paralela y distribuida AR",
+        #     "ingenieria de Software I BR",
+        #     "REDES BR",
+        #     "Fundamentos de programacion AR"
+        # ]
         contenedor_registro = QVBoxLayout()
         contenedor_registro.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
         widget_contenedor_registro = QWidget()
@@ -370,8 +372,8 @@ class inicio (QWidget):
                                     min-height: 60px;
                                     }""")
         anchomateria = int((self.ventana.width()*0.3252))
-        materia_input = QLineEdit()
-        materia_input.setStyleSheet(f"""QLineEdit{{
+        self.materia_input = QLineEdit()
+        self.materia_input.setStyleSheet(f"""QLineEdit{{
                             color: black;
                             font-family: sans-serif;
                             font-weight: bold;
@@ -382,6 +384,8 @@ class inicio (QWidget):
                             max-width: {anchomateria}px;
                             min-width: {anchomateria}px;
         }}""")
+        self.materia_input.setPlaceholderText(
+            "Ejemplo: Sistemas Inteligentes AR")
         contenedor_cabeza = QHBoxLayout()
         contenedor_cabeza.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
         widget_contenedor_cabeza = QWidget()
@@ -395,7 +399,7 @@ class inicio (QWidget):
         contenedor_cabeza.addWidget(
             texto_informacion_materia)
         contenedor_cabeza.addWidget(
-            materia_input, alignment=Qt.AlignmentFlag.AlignLeft)
+            self.materia_input, alignment=Qt.AlignmentFlag.AlignLeft)
 
         anchoTabla = int((self.ventana.width()*0.533))
         table = QTableWidget()
@@ -477,11 +481,17 @@ class inicio (QWidget):
         table.verticalHeader().setSectionResizeMode(
             QHeaderView.ResizeMode.ResizeToContents)
 
+    # todo Insetar informacion en la tabla ↓↓↓
         # for i in range(5):  # Rellena la tabla con datos de ejemplo
         #     for j in range(3):
         #         table.setItem(
         #             i, j, QTableWidgetItem(f"Celda {i+1}, {j+1}"))
-
+        # Primer bucle for para recorrer las sublistas (listas dentro de la lista principal)
+        for i in range(len(self.estudiantes)):
+            for j in range(3):
+                # print(self.estudiantes[i][j])
+                table.setItem(i, j, QTableWidgetItem(self.estudiantes[i][j]))
+    # todo Insertar informacion en la tabla ↑↑↑
         contenedor_form_curso.addWidget(
             widget_contenedor_cabeza, alignment=Qt.AlignmentFlag.AlignTop)
         contenedor_form_curso.addWidget(table)
@@ -525,8 +535,8 @@ class inicio (QWidget):
                             max-height: 20px;
                             min-height: 20px;
                         }""")
-        input_nombre_completo = QLineEdit()
-        input_nombre_completo.setStyleSheet("""QLineEdit{
+        self.input_nombre_completo = QLineEdit()
+        self.input_nombre_completo.setStyleSheet("""QLineEdit{
                             color: black;
                             font-family: sans-serif;
                             font-size: 15px;
@@ -537,7 +547,7 @@ class inicio (QWidget):
                             max-height: 50px;
                             min-height: 50px;
                         }""")
-        input_nombre_completo.setPlaceholderText(
+        self.input_nombre_completo.setPlaceholderText(
             "Escribe el nombre completo del estudiante")
 
         documento = QLabel("Numero de Documento")
@@ -554,10 +564,10 @@ class inicio (QWidget):
                             max-height: 20px;
                             min-height: 20px;
                         }""")
-        input_documento = QLineEdit()
-        input_documento.setPlaceholderText(
+        self.input_documento = QLineEdit()
+        self.input_documento.setPlaceholderText(
             "Escribe el documento del estudiante")
-        input_documento.setStyleSheet("""QLineEdit{
+        self.input_documento.setStyleSheet("""QLineEdit{
                             color: black;
                             font-family: sans-serif;
                             font-size: 15px;
@@ -582,10 +592,10 @@ class inicio (QWidget):
                             max-height: 20px;
                             min-height: 20px;
                         }""")
-        input_carrera = QLineEdit()
-        input_carrera.setPlaceholderText(
+        self.input_carrera = QLineEdit()
+        self.input_carrera.setPlaceholderText(
             "Escriba la carrera a la cual el estudiante pertenece")
-        input_carrera.setStyleSheet("""QLineEdit{
+        self.input_carrera.setStyleSheet("""QLineEdit{
                             color: black;
                             font-family: sans-serif;
                             font-size: 15px;
@@ -597,11 +607,11 @@ class inicio (QWidget):
                             min-height: 50px;
                         }""")
         contenedor_informacion.addWidget(nombre_completo)
-        contenedor_informacion.addWidget(input_nombre_completo)
+        contenedor_informacion.addWidget(self.input_nombre_completo)
         contenedor_informacion.addWidget(documento)
-        contenedor_informacion.addWidget(input_documento)
+        contenedor_informacion.addWidget(self.input_documento)
         contenedor_informacion.addWidget(carrera)
-        contenedor_informacion.addWidget(input_carrera)
+        contenedor_informacion.addWidget(self.input_carrera)
 
         contenedor_camara = QHBoxLayout()
         widget_contenedor_camara = QWidget()
@@ -844,7 +854,7 @@ class inicio (QWidget):
         return widget_contenedor_ayuda
 
     #! FUNCIONES DE VALIDACIONES ↓↓↓
-    # todo Funcion boton Registrar
+    # todo Funcion boton Registrar/Login
     def haz_dado_click(self):
         aprobacion = False
         usuario = int(self.usuario_input.text())
@@ -866,7 +876,26 @@ class inicio (QWidget):
             self.usuario_input.clear()
             self.Contra_input.clear()
 
+    # todo Funcion boton Registrar acomodar con lo WhatsApp ↓↓
+    def creacion_vector(self):
+
+        nombreEstudiante = self.input_nombre_completo.text()
+        documentoEstudiante = self.input_documento.text()
+        carreraEstudiante = self.input_carrera.text()
+        # print(nombreEstudiante)
+        # print(documentoEstudiante)
+        # print(carreraEstudiante)
+
+        datos_estudiante = [nombreEstudiante,
+                            documentoEstudiante, carreraEstudiante]
+
+        self.estudiantes.append(datos_estudiante)
+        # for estudiante in self.estudiantes:
+        #     print(estudiante)
+        pass
+
     #! FUNCIONES DE LOS BOTONES ↓↓ ¦ ↓↓ ¦ ↓↓ ¦
+    # ? funcion boton registrar del Login
     def cambiar_pantalla(self):
         self.contenedor_pre_registro.removeWidget(
             self.widget_contenedor_pre_pre_registro)
@@ -919,7 +948,16 @@ class inicio (QWidget):
         self.contenedor_pre_registro.removeWidget(self.widget_cuerpo_pag2)
         self.widget_cuerpo_pag2.hide()
         self.showMaximized()
-        self.widget_cuerpo.show()
+        nombre_clase = self.materia_input.text()
+        conexcionBD.insertar_clase(nombre_clase, self.profesor_id)
+        clase_id = conexcionBD.obtener_id_clase(nombre_clase)
+        for i in range(len(self.estudiantes)):
+            conexcionBD.insertar_estudiante(int(
+                self.estudiantes[i][1]), self.estudiantes[i][0], self.estudiantes[i][2], clase_id)
+        # conexcionBD.insertar_datos_biometricos(datos_biometricos, clase_id)
+        self.estudiantes = []
+        self.cambiar_pantalla()
+        # self.widget_cuerpo.show()
 
     # ?Funcion boton agregar estudiantes pag3
     def b_llenar_curso(self):
@@ -942,7 +980,8 @@ class inicio (QWidget):
         self.contenedor_pre_registro.removeWidget(self.widget_cuerpo_pag3)
         self.widget_cuerpo_pag3.hide()
         self.showMaximized()
-        self.widget_cuerpo_pag2.show()
+        self.crear_curso()
+        # self.widget_cuerpo_pag2.show()
 
     # ? Funcion boton cancelar pag4
     def volver_pag_cursos(self):
